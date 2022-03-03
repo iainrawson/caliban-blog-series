7f9379fb3a8c41ad2de1e50927810e805a9f7e53
Iain Rawson <iain.rawson@sseenergyservices.com>
2022-03-03 15:26:48 +0000

    Migrate to scala3/http4s

--- build.sbt
+++ build.sbt
@@ -2,13 +2,12 @@ name := "caliban-blog-series"
 
 version := "0.1"
 
-scalaVersion := "2.13.3"
+scalaVersion := "3.1.1"
 
 libraryDependencies ++= Seq(
-  "com.github.ghostdogpr"         %% "caliban"                       % "1.2.1",
-  "com.github.ghostdogpr"         %% "caliban-http4s"                % "1.2.1",
   "com.github.ghostdogpr"         %% "caliban-client"                % "1.2.1",
-  "com.softwaremill.sttp.client3" %% "async-http-client-backend-zio" % "3.3.15"
+  "com.softwaremill.sttp.client3" %% "http4s-backend"                % "3.5.0",
+  "org.http4s"                    %% "http4s-blaze-client"           % "0.23.10"
 )
 
 enablePlugins(CalibanPlugin)
--- src/main/scala/client/TrainApp.scala
+++ src/main/scala/client/TrainApp.scala
@@ -1,47 +1,40 @@
 package client
 
-import client.TrainClient._
-import sttp.client3._
-import sttp.client3.asynchttpclient.zio.{ send, AsyncHttpClientZioBackend }
-import zio.console.putStrLn
-import zio.{ App, ExitCode, ZIO }
+import client.TrainClient.{Query, Searchable, Station}
+import caliban.client.Operations.RootQuery
+import caliban.client.SelectionBuilder
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import sttp.capabilities.fs2.Fs2Streams
+import sttp.client3.*
+import sttp.client3.http4s._
+import cats.syntax.functor.*
 
-object TrainApp extends App {
+object TrainApp extends IOApp {
 
   case class Train(`type`: String, platform: String, trainNumber: String, time: String, stops: List[String])
 
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = {
-
-    val trainInStation =
-      (TrainInStation.`type` ~
-        TrainInStation.platform ~
-        TrainInStation.trainNumber ~
-        TrainInStation.time ~
-        TrainInStation.stops).mapN(Train)
+  def run(args: List[String]): IO[ExitCode] = {
 
     val query =
-      Query.search(Some("Berlin Ostbahnhof")) {
+      Query.search(Some("Berlin")) {
         Searchable.stations {
           Station.name ~
-            Station.hasWiFi ~
-            Station.timetable {
-              Timetable.nextDepatures {
-                trainInStation
-              } ~
-                Timetable.nextArrivals {
-                  trainInStation
-                }
-            }
+            Station.hasWiFi
         }
       }
 
     val uri = uri"https://api.deutschebahn.com/free1bahnql/v1/graphql"
 
-    send(query.toRequest(uri))
-      .map(_.body)
-      .absolve
-      .tap(res => ZIO.debug(s"Result: $res"))
-      .provideCustomLayer(AsyncHttpClientZioBackend.layer())
-      .exitCode
+    Http4sBackend.usingDefaultBlazeClientBuilder[IO]().use { sttpBackend =>
+      sttpBackend.send(query.toRequest(uri))
+    }.map { response =>
+      response.body.map( _.filter(_._2) )
+    }.flatMap { result =>
+      IO.delay {
+        println(result.fold(_.toString, _.toString))
+      }
+    }.as(ExitCode.Success)
+
   }
+
 }
